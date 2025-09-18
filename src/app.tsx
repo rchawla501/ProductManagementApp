408af25ba3cd429b0c18292abdb2ac085d6cd453
rchawla501 <rchawla501@users.noreply.github.com>
2025-09-18 21:29:35 +1000

    Deleted unused files

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,3 @@
-import { useState } from "preact/hooks";
-import preactLogo from "./assets/preact.svg";
-import viteLogo from "/vite.svg";
 import "./app.css";
 import Product from "./components/Product";
 
